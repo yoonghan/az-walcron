d81e93b76e98816683f7da65733cdfca4b00c449
bwars <b_wars@yahoo.com>
2026-04-08 17:25:41 +0800

    Add cors.

--- scripts/az-update-container.sh
+++ scripts/az-update-container.sh
@@ -4,3 +4,11 @@ az containerapp update \
   --name walcron \
   --resource-group walcron-rg \
   --image ghcr.io/yoonghan/az-walcron:latest
+
+az containerapp ingress cors enable \
+  --name walcron \
+  --resource-group walcron-rg \
+  --allowed-origins "https://www.walcron.com" "https://yoonghan.github.io" \
+  --allow-credentials true \
+  --allowed-methods "GET, POST, PUT, DELETE, OPTIONS" \
+  --allowed-headers "*"
