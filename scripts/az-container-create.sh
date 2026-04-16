823ffe8345baabe98c68d097167e3d2d88cd0ed2
han <mailyoonghan@gmail.com>
2026-04-17 07:22:33 +0800

    Add otel connection

--- scripts/az-container-create.sh
+++ scripts/az-container-create.sh
@@ -11,3 +11,11 @@ az containerapp create \
   --registry-server ghcr.io \
   --registry-username $1 \
   --registry-password $2
+
+# 3. Create OTEL connection
+az containerapp env telemetry app-insights set \
+  --name walcron-env \
+  --resource-group walcron-rg \
+  --connection-string "InstrumentationKey=deef89a4-0c02-417e-a0e7-be94a52d78a9;IngestionEndpoint=https://eastasia-0.in.applicationinsights.azure.com/;LiveEndpoint=https://eastasia.livediagnostics.monitor.azure.com/;ApplicationId=639678ac-5870-4688-bc6c-6324421753c1" \
+  --enable-open-telemetry-traces true \
+  --enable-open-telemetry-logs true
