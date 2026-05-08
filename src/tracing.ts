e756eac2bdecbb41eaba3fc3902976a8089f5e2e
han <b_wars@yahoo.com>
2026-05-09 07:56:58 +0800

    Declare resource to name tracing.

--- src/tracing.ts
+++ src/tracing.ts
@@ -3,6 +3,9 @@ import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-base";
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
 import { UndiciInstrumentation } from "@opentelemetry/instrumentation-undici";
+import { resourceFromAttributes } from "@opentelemetry/resources";
+import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from '@opentelemetry/semantic-conventions';
+
 
 // 1. Initialize Azure Monitor (for production/cloud)
 if (process.env.APPLICATIONINSIGHTS_CONNECTION_STRING) {
@@ -12,6 +15,10 @@ if (process.env.APPLICATIONINSIGHTS_CONNECTION_STRING) {
 // 2. We use our own NodeSDK to ADD the console exporter and extra instrumentations.
 // OpenTelemetry is smart enough to merge these configurations.
 const sdk = new NodeSDK({
+	resource: resourceFromAttributes({
+		[ATTR_SERVICE_NAME]: "az-walcron",
+		[ATTR_SERVICE_VERSION]: "1.0.0",
+	}),
 	traceExporter: new ConsoleSpanExporter(),
 	instrumentations: [
 		// No special config needed for basic propagation!
